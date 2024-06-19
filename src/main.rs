51f0e5f78a1a418e319431c0e878e311ea0de1ba
ikanago <me@ikanago.dev>
2024-06-19 22:38:28 +0900

    Return proper status code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use std::sync::{Arc, Mutex};
 
 use axum::extract::State;
+use axum::http::StatusCode;
 use axum::routing::get;
 use axum::Router;
 use scan::SensorData;
@@ -23,7 +24,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         let app = Router::new()
             .route("/metrics", get(handler))
-            .fallback(|| async { axum::http::StatusCode::NOT_FOUND })
+            .fallback(|| async { StatusCode::NOT_FOUND })
             .with_state(latest_metrics);
         let listener = TcpListener::bind(("0.0.0.0", 8080)).await.unwrap();
         axum::serve(listener, app).await.unwrap();
@@ -31,15 +32,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn handler(State(metrics): State<SharedSensorData>) -> String {
+async fn handler(State(metrics): State<SharedSensorData>) -> (StatusCode, String) {
     let metrics = metrics.lock().unwrap();
     let Some(metrics) = metrics.as_ref() else {
-        return String::from("value not available\n");
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            String::from("value not available\n"),
+        );
     };
 
     let body = format!(
         "battery {}\ntemperature {}\nhumidity {}\n",
         metrics.battery, metrics.temperature, metrics.humidity
     );
-    return body;
+    (StatusCode::OK, body)
 }
