23f9cde0853cc9a3b65074fd18fd45bd09e56fa6
ikanago <me@ikanago.dev>
2024-06-19 22:31:11 +0900

    Build a web server to cast sensor data

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,45 @@
 use std::error::Error;
-use std::str::FromStr;
+use std::sync::{Arc, Mutex};
 
-use btleplug::api::CentralEvent;
-use btleplug::api::{Central, Manager as _, ScanFilter};
-use btleplug::platform::Manager;
+use axum::extract::State;
+use axum::routing::get;
+use axum::Router;
+use scan::SensorData;
+use tokio::net::TcpListener;
 use tokio::runtime::Runtime;
-use tokio_stream::StreamExt;
-use uuid::Uuid;
+
+mod scan;
+
+type SharedSensorData = Arc<Mutex<Option<SensorData>>>;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let rt = Runtime::new()?;
     rt.block_on(async {
-        let manager = Manager::new().await.unwrap();
+        let latest_metrics = Arc::new(Mutex::new(None));
+        let latest_metrics_clone = latest_metrics.clone();
+        tokio::spawn(async move {
+            scan::scan_loop(latest_metrics_clone).await.unwrap();
+        });
 
-        // get the first bluetooth adapter
-        let adapters = manager.adapters().await?;
-        let central = adapters.into_iter().nth(0).unwrap();
-
-        // start scanning for devices
-        let mut events = central.events().await?;
-        central.start_scan(ScanFilter::default()).await?;
-
-        while let Some(event) = events.next().await {
-            if let CentralEvent::ServiceDataAdvertisement { service_data, .. } = event {
-                let now = chrono::Local::now();
-                println!("{} Service data: {:?}", now.to_string(), service_data);
-                let service_uuid = Uuid::from_str("0000fd3d-0000-1000-8000-00805f9b34fb")?;
-                if let Some(service_data) = service_data.get(&service_uuid) {
-                    if service_data.len() != 6 {
-                        continue;
-                    }
-                    let metrics = parse_service_data(&service_data);
-                    let now = chrono::Local::now();
-                    println!(
-                        "{} Battery: {}%, Temperature: {}°C, Humidity: {}%",
-                        now.to_string(),
-                        metrics.battery,
-                        metrics.temperature,
-                        metrics.humidity
-                    );
-                }
-            }
-        }
-        central.stop_scan().await?;
-
-        Ok(())
-    })
+        let app = Router::new()
+            .route("/metrics", get(handler))
+            .fallback(|| async { axum::http::StatusCode::NOT_FOUND })
+            .with_state(latest_metrics);
+        let listener = TcpListener::bind(("0.0.0.0", 8080)).await.unwrap();
+        axum::serve(listener, app).await.unwrap();
+    });
+    Ok(())
 }
 
-#[derive(Debug)]
-struct Metrics {
-    battery: u8,
-    temperature: f64,
-    humidity: u8,
-}
-
-fn parse_service_data(data: &[u8]) -> Metrics {
-    let battery = data[2] & 0b0111_1111;
-    let is_temperature_above_freezing = data[4] & 0b1000_0000 > 0;
-    let temperature = (data[3] & 0b0000_1111) as f64 * 0.1 + (data[4] & 0b0111_1111) as f64;
-    let temperature = if is_temperature_above_freezing {
-        temperature
-    } else {
-        -temperature
+async fn handler(State(metrics): State<SharedSensorData>) -> String {
+    let metrics = metrics.lock().unwrap();
+    let Some(metrics) = metrics.as_ref() else {
+        return String::from("value not available\n");
     };
-    let humidity = data[5] & 0b0111_1111;
 
-    Metrics {
-        battery,
-        temperature,
-        humidity,
-    }
+    let body = format!(
+        "battery {}\ntemperature {}\nhumidity {}\n",
+        metrics.battery, metrics.temperature, metrics.humidity
+    );
+    return body;
 }
---
+++ src/scan.rs
@@ -0,0 +1,67 @@
+use std::error::Error;
+use std::str::FromStr;
+use std::sync::{Arc, Mutex};
+
+use btleplug::api::CentralEvent;
+use btleplug::api::{Central, Manager as _, ScanFilter};
+use btleplug::platform::Manager;
+use tokio_stream::StreamExt;
+use uuid::Uuid;
+
+#[derive(Debug)]
+pub struct SensorData {
+    pub battery: f64,
+    pub temperature: f64,
+    pub humidity: f64,
+}
+
+pub async fn scan_loop(
+    latest_metrics: Arc<Mutex<Option<SensorData>>>,
+) -> Result<(), Box<dyn Error>> {
+    let manager = Manager::new().await.unwrap();
+
+    // get the first bluetooth adapter
+    let adapters = manager.adapters().await?;
+    let central = adapters.into_iter().nth(0).unwrap();
+
+    let mut events = central.events().await?;
+    central.start_scan(ScanFilter::default()).await?;
+
+    while let Some(event) = events.next().await {
+        if let CentralEvent::ServiceDataAdvertisement { service_data, .. } = event {
+            let service_uuid = Uuid::from_str("0000fd3d-0000-1000-8000-00805f9b34fb")?;
+            if let Some(service_data) = service_data.get(&service_uuid) {
+                if service_data.len() != 6 {
+                    continue;
+                }
+                let metrics = parse_service_data(&service_data);
+                println!(
+                    "Battery: {}%, Temperature: {}°C, Humidity: {}%",
+                    metrics.battery, metrics.temperature, metrics.humidity
+                );
+                *latest_metrics.lock().unwrap() = Some(metrics);
+            }
+        }
+    }
+    central.stop_scan().await?;
+
+    Ok(())
+}
+
+fn parse_service_data(data: &[u8]) -> SensorData {
+    let battery = (data[2] & 0b0111_1111) as f64;
+    let is_temperature_above_freezing = data[4] & 0b1000_0000 > 0;
+    let temperature = (data[3] & 0b0000_1111) as f64 * 0.1 + (data[4] & 0b0111_1111) as f64;
+    let temperature = if is_temperature_above_freezing {
+        temperature
+    } else {
+        -temperature
+    };
+    let humidity = (data[5] & 0b0111_1111) as f64;
+
+    SensorData {
+        battery,
+        temperature,
+        humidity,
+    }
+}
